f8333be14b3a1588d849e20ea4150ff888536ddc
Kate Skorija <kateskorija@gmail.com>
2020-07-24 10:03:58 -0700

    failing test for title property of order

--- BakeryTracker.Tests/ModelTests/OrderTests.cs
+++ BakeryTracker.Tests/ModelTests/OrderTests.cs
@@ -9,8 +9,19 @@ namespace BakeryTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("Test Order");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    [TestMethod]
+    public void GetTitle_ReturnsOrderTitle_String()
+    {
+      string title = "Test Order";
+      Order newOrder = new Order(title);
+
+      string result = newOrder.Title;
+
+      Assert.AreEqual(title, result);
+
     }
   }
 }
