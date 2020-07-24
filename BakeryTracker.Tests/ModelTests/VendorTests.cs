4d208610e33f83c6bc72396cefdc4ffd1b6bc8fe
Kate Skorija <kateskorija@gmail.com>
2020-07-24 09:24:13 -0700

    failing test for name property of vendor

--- BakeryTracker.Tests/ModelTests/VendorTests.cs
+++ BakeryTracker.Tests/ModelTests/VendorTests.cs
@@ -14,5 +14,15 @@ namespace BakeryTracker.Tests
       Vendor newVendor = new Vendor();
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+
+      string result = newVendor.Name;
+
+      Assert.AreEqual(name, result);
+    }
   }
 }
