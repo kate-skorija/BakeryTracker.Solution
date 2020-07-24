bbcef20bb73c102aec694a6f88d9e4efb3a729f8
Kate Skorija <kateskorija@gmail.com>
2020-07-24 09:53:51 -0700

    failing test for order constructor

---
+++ BakeryTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BakeryTracker.Models;
+
+namespace BakeryTracker.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
